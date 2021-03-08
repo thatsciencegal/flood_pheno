e3117725389702feccd9bdb96a41f2a1200285b5
Christine Swanson <christineswanson@ufl.edu>
2021-03-08 18:28:51 -0500

    added raster stacking code

--- Code/to_cloud_rem_prep.R
+++ Code/to_cloud_rem_prep.R
@@ -2,6 +2,9 @@
 ## a mask for the area of interest.
 
 library(raster)
+library(rgdal)
+library(ggplot2)
+library(tidyverse)
 
 ## Set the in and out directories
 in.dir <- "H:/SASA-PEAN/Raw/"
@@ -21,11 +24,11 @@ date.stack <- function(day){
   ## List files for a specific date
   l8.files1<-list.files(in.dir,pattern=paste0("LC08_L1TP_222069_",day,"*"),full.names = TRUE)
   ## Set the order for the bands
-  ord.names <- c("B1","B2","B3","B4","B5","B6","B7","B9","B10","B11")
+  ord.names <- c("B2","B3","B4","B5","B6")
   ##Create a raster stack of all the files
   rast <- stack(l8.files1)
   ## Rename the raster bands
-  names(rast)<-c("B1","B10","B11","B2","B3","B4","B5","B6","B7","B9")
+  names(rast)<-c("B2","B3","B4","B5","B6")
   ## Re-order the raster to have the bands in the correct order
   ord.rast <- rast[[ord.names]]
 }
@@ -39,11 +42,48 @@ for(i in 1:length(l8.splt.date)){
 ## Upload Mapbiomas data for water mask
 wtr.msk <- stack("D:/Dropbox/Dissertation/Chapter3/Data/MapbiomasVeg/sasa_pean_wtr_msk.tif")[[34]]
 
+## Reproject the raster to match Landsat projection
+wtr.msk.rpj <- projectRaster(wtr.msk,rast)
+
+## Resample water mask to match Landsat's spatial properties
+wtr.msk.rsp <- resample(wtr.msk.rc,rast)
+
 ## Reclassify water to 0 to create water mask
 rcl <- c(33,0)
 rcl.mtx <- matrix(rcl,ncol=2,byrow=TRUE)
 
-wtr.msk.rc<-reclassify(wtr.msk,rcl.mtx)
+wtr.msk.rc<-reclassify(wtr.msk.rsp,rcl.mtx)
+
+## Mask out Landsat no data values on water mask
+wtr.msk.rc.msk <- mask(wtr.msk.rsp,rast, maskvalue=0, updatevalue=NA)
 
 ## Write out water mask for year of interest
-writeRaster(wtr.msk.rc,"./Data/MapbiomasVeg/sasa_pean_wtr_msk_2018.tif")
+writeRaster(wtr.msk.rc.msk,"./Data/MapbiomasVeg/sasa_pean_wtr_msk_2018.tif",overwrite=TRUE)
+
+## Stack all the raster files together
+in.dir <- "H:/SASA-PEAN/Raw/Stacked"
+
+## List rasters in the directory
+l8.SS.PA<-list.files(in.dir,full.names=TRUE,pattern="tif$")
+
+## Read each day as a raster stack
+l8.ss.pa <- lapply(l8.SS.PA,stack)
+
+## Initiate the raster that will be used for resampling
+r <- l8.ss.pa[[1]]
+
+## Resample all the rasters
+for(i in 2:length(l8.ss.pa)){
+  r1 <- l8.ss.pa[[i]]
+  l8.ss.pa[[i]]<-resample(r1,r)
+}
+
+## Stack all the layers together
+x <- stack()
+for(i in 1:length(l8.ss.pa)){
+  rast <- l8.ss.pa[[1]]
+  x <- stack(x,rast)
+}
+
+## Write out the time series for the year
+writeRaster(x,"H:/SASA-PEAN/Raw/Stacked/2018_sasa_pean.tif")
